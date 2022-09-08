b60fe068f685037cf202acf79ee349013d23695a
pambahderick <pambahderick@gmail.com>
2022-09-08 13:40:37 -0700

    C program to print a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * puts() - Output characters and strings
+ * @myChar: character string variable
+ *
+ * Return: 0 (Success)
+ */
+int main()
+{
+	char myChar[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(myChar);
+	return 0;
+}
