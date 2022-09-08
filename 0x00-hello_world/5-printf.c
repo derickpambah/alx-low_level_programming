5e95d1f3366d846b67052205a04577b7248d9dbe
pambahderick <pambahderick@gmail.com>
2022-09-08 14:06:48 -0700

    Create C program to output a string using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char myString[56] = "with proper grammar, but the outcome is a piece of art,";
+	char myString[56] = "with proper grammar, but the outcome is a piece of art,\n";
 
 	printf("%s", myString);
 	return (0);
