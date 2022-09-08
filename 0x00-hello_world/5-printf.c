b673a4640e48139754e1ebee84e32177ffea5fdc
pambahderick <pambahderick@gmail.com>
2022-09-08 13:59:30 -0700

    Create a C program to output a string using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point to a c program
+ *
+ * Description: Program to print a string using printf
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char myString[55] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s", myString);
+	return (0);
+}
