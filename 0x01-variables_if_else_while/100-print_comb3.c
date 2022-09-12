4ec865e091e7b915b4cdbc104c9c828fc6124b88
pambahderick <pambahderick@gmail.com>
2022-09-11 22:18:00 -0700

    Print all possible combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+/**
+ * prin_t - Output integers using putchar
+ *
+ * @n: First set of digits to output
+ */
+void prin_t(int n)
+{
+	if (n / 10 != 0)
+		prin_t(n / 10);
+	putchar((n % 10) + '0');
+
+}
+
+/**
+ * prin_t2 - Output integers using putchar
+ *
+ * @m: Second set of digits to output
+ */
+void prin_t2(int m)
+{
+	if (m / 10 != 0)
+		prin_t2(m / 10);
+	putchar((m % 10) + '0');
+}
+
+/**
+ * main - Entry point
+ *
+ * Description: Print all possible combinations of two digits
+ * using only putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (m = n; m < 10; m++)
+		{
+			if (n < m)
+			{
+				prin_t(n);
+				prin_t2(m);
+
+				if(n != 8 || (n == 8 && m != 9))
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
