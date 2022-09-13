e8dca5f4b94a8de06e146eb27888da68d8572b8c
pambahderick <pambahderick@gmail.com>
2022-09-13 13:07:18 -0700

    Print lowercase alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print 10 times the alphabet
+ * @void: no arguments passed
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
