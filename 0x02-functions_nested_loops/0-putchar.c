748d21d97e7e7b4d1e27288b25cf3de02dcec307
pambahderick <pambahderick@gmail.com>
2022-09-13 04:28:37 -0700

    Edit Header file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: Print _putchar followed by a newline
+ * Return: On Success 1
+ * On error, -1 is returned, and errno is set appropriately
+ */
+int main(void)
+{
+	_putchar('_putchar');
+
+	return (0);
+}
