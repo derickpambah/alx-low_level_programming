6c7d2dd355c4cd973ffe5e8434575a1e65ea7a78
pambahderick <pambahderick@gmail.com>
2022-09-14 13:53:07 -0700

    Print the first 50 integers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -21,14 +21,14 @@ int main(void)
 		if (i != 49)
 		{
 			n3 = n1 + n2;
-			printf("%d, ", n3);
+			printf("%u, ", n3);
 			n1 = n2;
 			n2 = n3;
 		}
 		else
 		{
 			n3 = n1 + n2;
-			printf("%d", n3);
+			printf("%u", n3);
 		}
 	}
 	return (0);
