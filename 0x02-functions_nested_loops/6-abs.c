a9cd53f845889a5a1afb9ec71416712673ce5482
pambahderick <pambahderick@gmail.com>
2022-09-13 22:16:01 -0700

    Compute the absolute value of an integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,12 +2,13 @@
 
 /**
  * _abs - Compute the absolute value of an integer
+ * @c: Integer to be computed
  * Return: Absolute value of an integer
  */
 
 int _abs(int c)
 {
-	if (c == '-')
+	if (c < 0)
 		c = -c;
-	return c;
+	return (c);
 }
