9316a00d3f9feb6017dd9d22e438b6bfa32ffdbc
pambahderick <pambahderick@gmail.com>
2022-09-14 08:38:55 -0700

    Print the 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * times_table - Print the 9 times table starting with 0
+ * @void: No arguments passed
+ * Return: The 9 times table
+ */
+
+void times_table(void)
+{
+	d
+}
