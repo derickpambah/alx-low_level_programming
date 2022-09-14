9223f811937bfd1ebc6372bcc56fe38b7da1b30d
pambahderick <pambahderick@gmail.com>
2022-09-14 09:31:48 -0700

    Print the sum of all natural multiples of 3 and 5 in a range

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -14,12 +14,18 @@ int main(void)
 	int i;
 	int sum = 0;
 
-	for (i = 1; 1 < 1024; i++)
+	for (i = 1; i < 1024; i++)
 	{
 		if ((i % 3 == 0) || (i % 5 == 0))
 		{
 			sum += i;
 		}
+		else
+		{
+			continue;
+		}
 	}
 	printf("%d\n", sum);
+
+	return (0);
 }
