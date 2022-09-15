d66b68dcfd76eb973fa03932b25461bdd92f58a7
pambahderick <pambahderick@gmail.com>
2022-09-15 11:31:42 -0700

    Print an integer using putchar only

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,11 +14,9 @@ void print_number(int n)
 		n = -n;
 	}
 
-	if (n == 0)
-		_putchar('0');
-	
-	if (n / 10)
+	if (n / 10 != 0)
 		print_number(n / 10);
 
 	_putchar((n % 10) + '0');
+	_putchar('\n');
 }
