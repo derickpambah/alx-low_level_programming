528257071868b49087d96f8ef56b2131e31f4d95
pambahderick <pambahderick@gmail.com>
2022-09-21 04:55:09 -0700

    Print a string in reverse followed by a new line

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * _strlen - return the length of a string
+ * @s: String
+ *
+ * Return: int (length of a string)
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
+
+/**
  * print_rev - Print a string in reverse
  * @s: String
  *
@@ -8,11 +26,24 @@
  */
 void print_rev(char *s)
 {
-	while (*s != '\0')
-	{
-		char ch = (char) *s;
+	int length, i;
+	char *begin, *end, ch;
 
-		_putchar(ch);
-		s--;
+	length = _strlen(s);
+
+	begin = s;
+	end = s;
+
+	for (i = 0; i < (length - 1); i++)
+		end++;
+
+	for (i = 0; i < (length / 2); i++)
+	{
+		ch = *end;
+		*end = *begin;
+		*begin = ch;
+
+		begin++;
+		end--;
 	}
 }
